2c7993c90f5db442b9d83b8ec330390ed8d45ba7
Volkova Anastasiya <avftiq@gmail.com>
2022-11-12 03:26:54 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.listen(PORT, () => {
+  console.log(`Listen port: ${PORT}`);
+});
