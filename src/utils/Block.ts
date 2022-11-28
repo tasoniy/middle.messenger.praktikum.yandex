5411f18bd1bd2e72b17bc930ff3c1b032ba3385d
Volkova Anastasiya <avftiq@gmail.com>
2022-11-28 12:00:55 +0300

    add directory utils with files Block.ts and EventBus.ts

---
+++ src/utils/Block.ts
@@ -0,0 +1,15 @@
+import { EventBus } from "./EventBus";
+import { nanoid } from "nanoid";
+
+class Block {
+  static EVENTS = {
+    INIT: "init",
+    FLOW_CDM: "flow-component-did-mount",
+    FLOW_CDU: "flow-component-did-update",
+    FLOW_RENDER: "flow-reender",
+  };
+
+  public id = nanoid(6)
+
+  private _element: HTMLElement | null = null;
+}
